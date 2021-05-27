f13a7372cd0fa7cb33d95a2dd7f6376ea3c36539
Chip Long <chip_long@yahoo.com>
2021-05-26 19:04:18 -0600

    added model index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
-
+Category.hasMany(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  unique: false,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Products, {
+  through: ProductTag,
+  unique: false,
+});
 
 module.exports = {
   Product,
