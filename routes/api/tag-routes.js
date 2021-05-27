0665c01ca92ccd0694e38d6a24d10dea53d4f4a5
Chip Long <chip_long@yahoo.com>
2021-05-26 22:06:31 -0600

    tag routes are working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,9 @@ router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagList = await Tag.findAll();
+    const tagList = await Tag.findAll({
+      include: [{ model: Product }],
+    });
     if (tagList.length === 0) {
       res.status(404).json({ message: "No tags found." });
       return;
@@ -19,9 +21,22 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagList = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (tagList.length === 0) {
+      res.status(404).json({ message: "No tags found." });
+      return;
+    }
+    res.status(200).json(tagList);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", async (req, res) => {
@@ -38,12 +53,9 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tag = await Tag.update(
-      { tag_name: req.body.tag_name },
-      {
-        where: { id: req.params.id },
-      }
-    );
+    const tag = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
     res.status(200).json(tag);
   } catch (err) {
     console.log(err);
@@ -53,6 +65,15 @@ router.put("/:id", async (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = Tag.destroy({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(deletedTag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
