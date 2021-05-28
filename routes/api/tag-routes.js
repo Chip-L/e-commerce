deed29c2ac05c411cc046a5ec88254df85525a6d
Chip Long <chip_long@yahoo.com>
2021-05-27 18:47:51 -0600

    fixed some errors and added some error checking.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     res.status(200).json(tagList);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(500).json({ error: "Check server logs" });
   }
 });
 
@@ -35,44 +35,52 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(tagList);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(500).json({ error: "Check server logs" });
   }
 });
 
 router.post("/", async (req, res) => {
   // create a new tag
   try {
+    if (!req.body.tag_name) {
+      res.status(406).json({ message: "tag_name cannot be NULL." });
+      return;
+    }
     const newTag = await Tag.create(req.body);
     res.status(200).json(newTag);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(500).json({ error: "Check server logs" });
   }
 });
 
 router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
+    if (!req.body.tag_name) {
+      res.status(406).json({ message: "tag_name cannot be NULL." });
+      return;
+    }
     const tag = await Tag.update(req.body, {
       where: { id: req.params.id },
     });
     res.status(200).json(tag);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(500).json({ error: "Check server logs" });
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const deletedTag = Tag.destroy({
+    const deletedTag = await Tag.destroy({
       where: { id: req.params.id },
     });
     res.status(200).json(deletedTag);
   } catch (err) {
     console.log(err);
-    res.status(500).json(err);
+    res.status(500).json({ error: "Check server logs" });
   }
 });
 
